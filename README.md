998a1cf3ec3feb1a2f95e4e41a813b3a62bf1205
UTB400 <129385288+UWVOT400@users.noreply.github.com>
2024-10-30 17:52:29 +0400

    marinegpt_commit

--- README.md
+++ README.md
@@ -1,1 +1,43 @@
-# AquaticCLIP
+# AquaticCLIP: A Vision-Language Foundation Model for Underwater Scene Analysis
+
+![AquaticCLIP](./images/model.png)
+
+
+### Abstract
+The preservation of aquatic biodiversity is critical in mitigating the effects of climate change. Aquatic scene understanding plays a pivotal role in aiding marine scientists in their decision-making processes. In this paper, we introduce AquaticCLIP, a novel contrastive language-image pre-training model tailored for aquatic scene understanding. AquaticCLIP presents a new unsupervised learning framework that aligns images and text in aquatic environments, enabling tasks such as segmentation, classification, detection, and object counting. By leveraging our large-scale underwater image-text paired dataset without the need for ground-truth annotations, our model enriches existing vision-language models in the aquatic domain. For this purpose, we construct a 2 million underwater image-text paired dataset using heterogeneous resources including YouTube, Netflix, NatGeo, etc. To fine-tune AquaticCLIP, we propose a prompt-guided vision encoder that progressively aggregates patch features via learnable prompts, while a vision-guided mechanism enhances the language encoder by incorporating visual context. The model is optimized through a contrastive pre-training loss to align visual and textual modalities. AquaticCLIP achieves notable performance improvements in zero-shot settings across multiple underwater computer vision tasks, outperforming existing methods in both robustness and interpretability. Our model sets a new benchmark for vision-language applications in underwater environments. 
+
+## Environment Setup 
+
+This setup is tested only on Linux.
+
+1. Clone this repository and navigate to AquaticCLIP
+
+```
+git clone https://github.com/BasitAlawode/AquaticCLIP.git AquaticCLIP
+cd AquaticCLIP
+```
+
+2. Create environment and install dependencies
+
+```
+conda env create -f environment.yml
+conda activate aquaticclip
+```
+
+## Text Generation with MarineGPT
+
+3. Download the Vicuna-7b model weights from [here](https://huggingface.co/Vision-CAIR/vicuna-7b/tree/main) and put them in side the vicuna-7b folder. NOTE: all files (8 in total) 
+in this link should be downloaded.
+
+4. Download MarineGPTVicuna 7B stage 2 model from [here](https://www.dropbox.com/scl/fi/8uimfr9vjk8sa6yyvvnbk/marinegpt_vicuna_7B_stage2_ckpt.pth?rlkey=4cwn4cmgi8gjnqfyds2aqnw8s&e=1&dl=0) and put it inside the marinegpt_weight folder. NOTE: This weights are obtained from marinegpt repository.
+
+5. Run the below file to generate captions for our sample images (located in the images folder).
+
+```
+python generate_captions_for_imgs.py
+```
+
+
+## Acknowledgement
+ - A part of our work is based on [MarineGPT](https://github.com/hkust-vgd/MarineGPT).
+
---
+++ generate_captions_for_imgs.py
@@ -0,0 +1,100 @@
+import argparse
+import glob
+import os
+import json
+import random
+import numpy as np
+import torch
+import torch.backends.cudnn as cudnn
+import gradio as gr
+import pycocotools.mask as maskUtils
+from marinegpt.common.config import Config
+from marinegpt.common.dist_utils import get_rank
+from marinegpt.common.registry import registry
+from marinegpt.conversation.conversation import Chat, CONV_VISION
+from PIL import Image
+# imports modules for registration
+from marinegpt.datasets.builders import *
+from marinegpt.models import *
+from marinegpt.processors import *
+from marinegpt.runners import *
+from marinegpt.tasks import *
+
+# import spacy
+# nlp = spacy.load('en_core_web_sm')
+# def get_noun_phrases(text):
+#     doc = nlp(text)
+#     noun_phrases = []
+#     for chunk in doc.noun_chunks:
+#         noun_phrases.append(chunk.text)
+#     return noun_phrases
+
+def parse_args():
+    parser = argparse.ArgumentParser(description="Demo")
+    parser.add_argument("--cfg-path", type=str, default="eval_configs/marinegpt_eval.yaml", help="path to configuration file.")
+    parser.add_argument("--gpu-id", type=int, default=0, help="specify the gpu to load the model.")
+    parser.add_argument("--img_path", type=str, default="test_images", help="path of images")
+    parser.add_argument("--model_type", type=str, default="llama_model", help="Could be one of llama_model or gemma_model")
+    parser.add_argument(
+        "--options",
+        nargs="+",
+        help="override some settings in the used config, the key-value pair "
+             "in xxx=yyy format will be merged into config file (deprecate), "
+             "change to --cfg-options instead.",
+    )
+    args = parser.parse_args()
+    return args
+
+
+def setup_seeds(config):
+    seed = config.run_cfg.seed + get_rank()
+
+    random.seed(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+
+    cudnn.benchmark = False
+    cudnn.deterministic = True
+
+# ========================================
+#             Model Initialization
+# ========================================
+
+print('Initializing Chat')
+args = parse_args()
+cfg = Config(args)
+
+model_config = cfg.model_cfg
+
+# Added by me
+args.img_path = "MarineCLIP_images"
+model_config.model_format = args.model_type
+
+model_config.device_8bit = args.gpu_id
+model_cls = registry.get_model_class(model_config.arch)
+model = model_cls.from_config(model_config).to('cuda:{}'.format(args.gpu_id))
+
+vis_processor_cfg = cfg.datasets_cfg.cc_sbu_align.vis_processor.train
+vis_processor = registry.get_processor_class(vis_processor_cfg.name).from_config(vis_processor_cfg)
+chat = Chat(model, args.model_type, vis_processor, device='cuda:{}'.format(args.gpu_id))
+print('Initialization Finished')
+
+#for file in glob.glob(os.path.join(args.img_path,"*.jpg")):
+for file in [f"{args.img_path}/{f}" for f in os.listdir(args.img_path) if not f.endswith('.txt')]:
+    chat_state = CONV_VISION.copy()
+    #chat_state.messages = [['Human', '<Img><ImageHere></Img> Describe the object in this figure']]
+    chat_state.messages = [['Human', '<Img><ImageHere></Img> Describe this figure in detail']]
+    img_list = []
+    
+    fname, ext = os.path.splitext(file)
+    if os.path.exists(file.replace(ext,".txt")):
+        continue
+    write_data=open(file.replace(ext,".txt"),"w")
+    patch_large=Image.open(file).convert('RGB')
+    captions=chat.generate_caption(patch_large,chat_state,img_list)
+    write_data.write(captions)
+    write_data.close()
+    
+    print("=====================================")
+    print(f"Image: {fname} \n")
+    print(captions)
